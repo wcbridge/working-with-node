db293b6a8ffad2b9bcb0551f296578d05020fa65
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-04-28 10:17:51 -0400

    some spotify

--- liri.js
+++ liri.js
@@ -1,24 +1,55 @@
+//import { domainToASCII } from "url";
+
 require("dotenv").config();
+var keys = require('./keys.js');
 var fs = require('fs');
 var request = require("request");
+var Spotify = require('node-spotify-api');
+ var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
+ var userInput = process.argv[2];
+ var song = process.argv[3];
+ switch(userInput) {
+     case 'my-tweets':
+        displayTweet();
+        break;
+     case 'spotify-this-song':
+        spotifySong(song);
+        break;
+    case 'do-what-it-says':
+        doIt()
+        break;
+    default:
+        console.log('Im sorry, I dont understand that command!');    
+}
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+function spotifySong(song) {
+    var static = 'The sign';
+    spotify.search({ type: 'track', query:'livin on a prayer', limit:1 }, function (err,data) {
+     
+        console.log(data.tracks.items[0].name)
+        console.log(data.tracks.items[0].artists[0].name)
 
-//omdb
-var movieName = process.argv[2]
+        
+    }) 
+}
 
-var omdb = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-request(omdb, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
 
-        console.log("Title: " + JSON.parse(body).Title);
-        console.log("Release Year: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-        console.log("Country: " + JSON.parse(body).Country);
-        console.log("Language: " + JSON.parse(body).Language);
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log("Starring: " + JSON.parse(body).Actors);
-    }
-});
+
+// //omdb 
+// var movieName = process.argv[2]
+
+// var omdb = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// request(omdb, function (error, response, body) {
+//     if (!error && response.statusCode === 200) {
+
+//         console.log("Title: " + JSON.parse(body).Title);
+//         console.log("Release Year: " + JSON.parse(body).Year);
+//         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+//         console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+//         console.log("Country: " + JSON.parse(body).Country);
+//         console.log("Language: " + JSON.parse(body).Language);
+//         console.log("Plot: " + JSON.parse(body).Plot);
+//         console.log("Starring: " + JSON.parse(body).Actors);
+//     }
+// });
--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-// spotify
+// //omdb
+// var request = require("request");
+// var movieName = process.argv[2]
+
+// var omdb = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// //var omdb = "http://www.omdbapi.com/?t=&y=&plot=short&apikey=trilogy";
+
+
+// request(omdb, function (error, response, body) {
+
+//     // If the request is successful
+//     if (!error && response.statusCode === 200) {
+
+//         // Parse the body of the site and recover just the imdbRating
+//         // // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+//         console.log("Title: " + JSON.parse(body).Title);
+//         console.log("Release Year: " + JSON.parse(body).Year);
+//         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+//         console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+//         console.log("Country: " + JSON.parse(body).Country);
+//         console.log("Language: " + JSON.parse(body).Language);
+//         console.log("Plot: " + JSON.parse(body).Plot);
+//         console.log("Starring: " + JSON.parse(body).Actors);
+//     }
+// });
+//spotify
+
+
+//Twitter
+
+
+
